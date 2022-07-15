2c2f11a47b3e54078ac5e23a8b2ce06d0ed872cb
Rajesh Thatha <rthatha@gmail.com>
2022-07-15 16:37:23 +0200

    show dataframe

--- successstories.py
+++ successstories.py
@@ -9,36 +9,23 @@ import streamlit as st
 """
 
 
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
-
-    Point = namedtuple('Point', 'x y')
-    data = []
-
-    points_per_turn = total_points / num_turns
-
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
-
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
-
-
-
 @st.cache
 def get_data():
-path = r'cars.csv'
-return pd.read_csv(path)
-df = get_data()
+    path = r'cars.csv'
+    return pd.read_csv(path)
+
+df = pd.read_csv("./data/Summary.csv")
+
+df
 
 makes = df['make'].drop_duplicates()
+years = df['year']
+models = df['model']
+engines = df['engine']
+components = df['components']
 make_choice = st.sidebar.selectbox('Select your vehicle:', makes)
-years = df["year"].loc[df["make"] = make_choice]
-year_choice = st.sidebar.selectbox('', years) 
+year_choice = st.sidebar.selectbox('', years)
+model_choice = st.sidebar.selectbox('', models)
+engine_choice = st.sidebar.selectbox('', engines)
+
+st.write('Results:', components)
