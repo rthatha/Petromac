4e6377641aef8d99d26143c6bb517236b67349d3
Rajesh Thatha <rthatha@gmail.com>
2022-07-15 16:45:45 +0200

    debug3

--- successstories.py
+++ successstories.py
@@ -21,9 +21,9 @@ df
 wlco = df['WL Co'].drop_duplicates()
 area = df['Area'].drop_duplicates()
 country = df['Country'].drop_duplicates()
-categories = df['Category 1'].drop_duplicates
+categories = df['Category 1'].drop_duplicates()
 wlco_choice = st.sidebar.selectbox('Wl Co:', wlco)
 area_choice = st.sidebar.selectbox('Area:', area)
 country_choice = st.sidebar.selectbox('Country:', country)
-categories_choice = st.sidebar.selectbox('', categories)
+categories_choice = st.sidebar.selectbox('Categories:', categories)
 
