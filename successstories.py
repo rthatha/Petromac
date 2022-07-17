4404b9a2669e02c415b4d6d8b63c56fcc0133f36
Rajesh Thatha <rthatha@gmail.com>
2022-07-17 12:04:43 +0200

    first refactor

--- successstories.py
+++ successstories.py
@@ -12,15 +12,21 @@ import numpy as np
 
 @st.cache
 def get_data():
-    path = r'cars.csv'
-    return pd.read_csv(path)
+    df_casestudies = pd.read_csv("./data/Summary.csv")
+    return df_casestudies
 
-df = pd.read_csv("./data/Summary.csv")
 
-df
+df_casestudies = get_data()
+df_casestudies
 
-wlco = df['WL Co'].drop_duplicates()
-wlco_choice = st.sidebar.selectbox('Wl Co:', wlco)
+wlcos = df_casestudies['WL Co'].drop_duplicates()
+areas = df_casestudies["Area"].drop_duplicates()
+countries = df_casestudies['Country'].drop_duplicates()
+categories = df_casestudies['Category 1'].drop_duplicates()
+
+
+
+wlco_choice = st.sidebar.selectbox('Wl Co:', (wlcos,'All'),on_change = None)
 
 
 area = df["Area"].loc[df['WL Co'] == wlco_choice].drop_duplicates()
