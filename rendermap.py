3aa43dd5fb5c58be891266b0565f677d446d5b5f
Rajesh Thatha <rthatha@gmail.com>
2022-07-17 16:52:01 +0200

    import pdk

--- rendermap.py
+++ rendermap.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+import pydeck as pdk
 
 def rendermap():
     df = pd.DataFrame(
--- successstories.py
+++ successstories.py
@@ -58,38 +58,9 @@ categories = df['Category 1'].drop_duplicates()
 
 
 """
-df = pd.DataFrame(
-    np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
-    columns=['lat', 'lon'])
+import rendermap
+rendermap()
 
-st.pydeck_chart(pdk.Deck(
-     map_style='mapbox://styles/mapbox/light-v9',
-     initial_view_state=pdk.ViewState(
-         latitude=37.76,
-         longitude=-122.4,
-         zoom=11,
-         pitch=50,
-     ),
-     layers=[
-         pdk.Layer(
-            'HexagonLayer',
-            data=df,
-            get_position='[lon, lat]',
-            radius=200,
-            elevation_scale=4,
-            elevation_range=[0, 1000],
-            pickable=True,
-            extruded=True,
-         ),
-         pdk.Layer(
-             'ScatterplotLayer',
-             data=df,
-             get_position='[lon, lat]',
-             get_color='[200, 30, 0, 160]',
-             get_radius=200,
-         ),
-     ],
- ))
 
 map_data = pd.read_csv("./data/countries1.csv")
 map_data
