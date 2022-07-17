38c064b0ad1b99b1ecd1672f343127a4e31e9225
Rajesh Thatha <rthatha@gmail.com>
2022-07-17 16:46:10 +0200

    rendermap

--- successstories.py
+++ successstories.py
@@ -58,31 +58,8 @@ categories = df['Category 1'].drop_duplicates()
 
 
 """
-
-data = pd.DataFrame({
-    'awesome cities' : ['Chicago', 'Minneapolis', 'Louisville', 'Topeka'],
-    'lat' : [41.868171, 44.979840,  38.257972, 39.030575],
-    'lon' : [-87.667458, -93.272474, -85.765187,  -95.702548]
-})
-
-# Adding code so we can have map default to the center of the data
-midpoint = (np.average(data['lat']), np.average(data['lon']))
-
-st.map(
-            viewport={
-                'latitude': midpoint[0],
-                'longitude':  midpoint[1],
-                'zoom': 4
-            },
-            layers=[{
-                'type': 'ScatterplotLayer',
-                'data': data,
-                'radiusScale': 250,
-   'radiusMinPixels': 5,
-                'getFillColor': [248, 24, 148],
-            }]
-        )
-
+from rendermap import rendermap
+rendermap()
 
 map_data = pd.read_csv("./data/countries1.csv")
 map_data
