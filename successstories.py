48280815e99fd5149d4bd4674ee268008ff96860
Rajesh Thatha <rthatha@gmail.com>
2022-07-17 16:37:06 +0200

    map trials

--- successstories.py
+++ successstories.py
@@ -61,12 +61,7 @@ categories = df['Category 1'].drop_duplicates()
 
 
 
-map_data = pd.DataFrame(
-    np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
-    columns=['lat', 'lon'])
-map_data = pd.read_csv("./data/countries1.csv")
-map_data
-st.map(map_data)
+
 
 data = pd.DataFrame({
     'awesome cities' : ['Chicago', 'Minneapolis', 'Louisville', 'Topeka'],
@@ -91,3 +86,8 @@ st.deck_gl_chart(
                 'getFillColor': [248, 24, 148],
             }]
         )
+
+
+map_data = pd.read_csv("./data/countries1.csv")
+map_data
+st.map(map_data)
