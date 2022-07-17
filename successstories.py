217d41ae581f9ff47b8317c1d6b257644121310e
Rajesh Thatha <rthatha@gmail.com>
2022-07-17 11:50:24 +0200

    filter button?

--- successstories.py
+++ successstories.py
@@ -17,28 +17,24 @@ def get_data():
 
 df = pd.read_csv("./data/Summary.csv")
 
-st.write(df)
-st.dataframe(df)
+df
 
 wlco = df['WL Co'].drop_duplicates()
 wlco_choice = st.sidebar.selectbox('Wl Co:', wlco)
 
-df[df['WL Co'] == wlco_choice]
 
 area = df["Area"].loc[df['WL Co'] == wlco_choice].drop_duplicates()
 area_choice = st.sidebar.selectbox('Area:', area)
-df[df['Area'] == area_choice]
 
 country = df['Country'].loc[df['WL Co'] == wlco_choice].loc[df['Area'] == area_choice].drop_duplicates()
 country_choice = st.sidebar.selectbox('Country:', country)
-df[df['Country'] == country_choice]
+
 
 categories = df['Category 1'].drop_duplicates()
 
-
-
 categories_choice = st.sidebar.selectbox('Categories:', categories)
 
+df[(df['WL Co'] == wlco_choice) & (df['Area'] == area_choice) & (df['Country'] == country_choice) & (df['Category 1'] == categories_choice)]
 
 chart_data = pd.DataFrame(
      np.random.randn(20, 3),
@@ -51,3 +47,10 @@ map_data = pd.DataFrame(
     columns=['lat', 'lon'])
 
 st.map(map_data)
+
+if st.checkbox('Show dataframe'):
+    chart_data = pd.DataFrame(
+       np.random.randn(20, 3),
+       columns=['a', 'b', 'c'])
+
+    chart_data
