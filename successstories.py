db923db3705a7660bf518f6ea3ce198998aed5cd
Rajesh Thatha <rthatha@gmail.com>
2022-07-17 11:40:00 +0200

    charts

--- successstories.py
+++ successstories.py
@@ -39,3 +39,14 @@ categories = df['Category 1'].drop_duplicates()
 categories_choice = st.sidebar.selectbox('Categories:', categories)
 
 
+chart_data = pd.DataFrame(
+     np.random.randn(20, 3),
+     columns=['a', 'b', 'c'])
+
+st.line_chart(chart_data)
+
+map_data = pd.DataFrame(
+    np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
+    columns=['lat', 'lon'])
+
+st.map(map_data)
