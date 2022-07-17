bf31fc2fe450ceba4a4b897fbb8869418486e8a8
Rajesh Thatha <rthatha@gmail.com>
2022-07-17 16:36:55 +0200

    map trials

--- successstories.py
+++ successstories.py
@@ -59,11 +59,7 @@ categories = df['Category 1'].drop_duplicates()
 
 """
 
-chart_data = pd.DataFrame(
-     np.random.randn(20, 3),
-     columns=['a', 'b', 'c'])
 
-st.line_chart(chart_data)
 
 map_data = pd.DataFrame(
     np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
@@ -72,9 +68,26 @@ map_data = pd.read_csv("./data/countries1.csv")
 map_data
 st.map(map_data)
 
-if st.checkbox('Show dataframe'):
-    chart_data = pd.DataFrame(
-       np.random.randn(20, 3),
-       columns=['a', 'b', 'c'])
+data = pd.DataFrame({
+    'awesome cities' : ['Chicago', 'Minneapolis', 'Louisville', 'Topeka'],
+    'lat' : [41.868171, 44.979840,  38.257972, 39.030575],
+    'lon' : [-87.667458, -93.272474, -85.765187,  -95.702548]
+})
 
-    chart_data
+# Adding code so we can have map default to the center of the data
+midpoint = (np.average(data['lat']), np.average(data['lon']))
+
+st.deck_gl_chart(
+            viewport={
+                'latitude': midpoint[0],
+                'longitude':  midpoint[1],
+                'zoom': 4
+            },
+            layers=[{
+                'type': 'ScatterplotLayer',
+                'data': data,
+                'radiusScale': 250,
+   'radiusMinPixels': 5,
+                'getFillColor': [248, 24, 148],
+            }]
+        )
